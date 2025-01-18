5ca01a4310d8103062b6cbc2a3678f740c15e564
Manik <manikchauhan029@gmail.com>
2025-01-18 16:33:55 +0530

    Signup page

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const path=require('path');
 const express=require('express');
+const mongoose=require('mongoose');
+
+
 const app=express();
 const port=3000;
+
+mongoose.connect('mongodb://localhost:27017/blogify').
+then((e)=>console.log('Connected to DB')).
+catch((err)=>console.log(err));
+
+const userRoute=require('./routes/user');
 
 app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
@@ -10,6 +19,8 @@ app.get('/',(req,res)=>{
     return res.render('home');
 });
 
+app.use("/users",userRoute);
+app.use(express.urlencoded({extended:true}));
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const {createHmac , randomBytes}=require('node:crypto');
 const { Schema, model }=require('mongoose');
 
 const userSchema=new Schema({
@@ -11,8 +12,7 @@ const userSchema=new Schema({
         unique:true
     },
     salt:{
-        type:String,
-        required:true
+        type:String
     },
     password:{
         type:String,
@@ -31,4 +31,20 @@ const userSchema=new Schema({
     {timestamps:true}
 );
 
+userSchema.pre('save',function(next){
+    if(!this.isModified('password')) return ;
+    const salt=randomBytes(16).toString('hex');
+    const hashedPassword=createHmac('sha256',salt).update(this.password).digest('hex');
+    this.salt=salt;
+    this.password=hashedPassword;
+    next();
+});
+// The pre('save') middleware function in Mongoose relies on this to refer to the current document being
+// saved (i.e., the user document in this case).
+// If you use an arrow function, the value of this will not refer to the document; instead,
+// it will inherit this from the enclosing scope, which is likely not what you want.
+
+
 const User=model('User',userSchema);
+
+module.exports=User;
